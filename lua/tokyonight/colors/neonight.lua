f13e853e60bfd03b836cfd63e4bd7c0d7ec14906
SeeTwoo <waltibee@gmail.com>
2026-08-29 20:42:24 +0200

    save

---
+++ lua/tokyonight/colors/neonight.lua
@@ -0,0 +1,73 @@
+local dark_blue     = "#000717"
+local dark_blue1    = "#00091e"
+local dark_blue2    = "#041230"
+local dark_blue3    = "#010b21"
+local drak_blue4    = "#1a2c55"
+local white         = "#faf0fa"
+local light_blue    = "LightBlue"
+local deep_pink     = "DeepPink"
+local cyan          = "Cyan"
+local steel_blue    = "SteelBlue"
+local lavender      = "Lavender"
+local plum          = "Plum"
+local thistle       = "Thistle"
+local violet        = "Violet"
+local dark_grey     = "#001022"
+--[[
+return {
+  bg0       = dark_blue,
+  bg1       = dark_blue3,
+  bg2       = dark_blue2,
+  bg3       = dark_blue4,
+  fg0       = "#faf0fa",        
+  fg1       = "#e6e6fa",
+  fg2       = "#dda0dd",
+  fg3       = "#d8bfd8",
+  fg4       = "#ee82ee",
+  fg5       = "#add8e6",
+  accent0   = "#ff1493",
+  accent1   = "#00ffff",
+  borders   = "#add8e6",
+  muted     = "#4682b4",
+}
+]]
+
+local ret = {
+  bg = dark_blue,
+  bg_dark = "#1f2335",
+  bg_dark1 = "#1b1e2d",
+  bg_highlight = "#292e42",
+  blue = "#7aa2f7",
+  blue0 = "#3d59a1",
+  blue1 = "#2ac3de",
+  blue2 = "#0db9d7",
+  blue5 = "#89ddff",
+  blue6 = "#b4f9f8",
+  blue7 = "#394b70",
+  comment = "#565f89",
+  cyan = "#7dcfff",
+  dark3 = "#545c7e",
+  dark5 = "#737aa2",
+  fg = "#c0caf5",
+  fg_dark = "#a9b1d6",
+  fg_gutter = "#3b4261",
+  green = "#9ece6a",
+  green1 = "#73daca",
+  green2 = "#41a6b5",
+  magenta = "#bb9af7",
+  magenta2 = "#ff007c",
+  orange = "#ff9e64",
+  purple = "#9d7cd8",
+  red = "#f7768e",
+  red1 = "#db4b4b",
+  teal = "#1abc9c",
+  terminal_black = "#414868",
+  yellow = "#e0af68",
+  git = {
+    add = "#449dab",
+    change = "#6183bb",
+    delete = "#914c54",
+  },
+}
+
+return ret
