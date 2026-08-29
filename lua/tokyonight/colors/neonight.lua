c7b8f2d19c6ed0e7e6c7b54432874f7433192715
SeeTwoo <waltibee@gmail.com>
2026-08-30 00:14:11 +0200

    great neonight palette omg ??

--- lua/tokyonight/colors/neonight.lua
+++ lua/tokyonight/colors/neonight.lua
@@ -1,73 +1,52 @@
-local dark_fg1     = "#000717"
-local dark_fg11    = "#00091e"
-local dark_fg12    = "#041230"
-local dark_fg13    = "#010b21"
-local drak_fg14    = "#1a2c55"
-local white         = "#faf0fa"
-local light_fg1    = "LightBlue"
-local deep_pink     = "DeepPink"
-local fg_pop2          = "Cyan"
-local steel_fg1    = "SteelBlue"
-local lavender      = "Lavender"
-local plum          = "Plum"
-local thistle       = "Thistle"
-local violet        = "Violet"
-local dark_grey     = "#001022"
---[[
-return {
-  bg0       = dark_fg1,
-  bg1       = dark_fg13,
-  bg2       = dark_fg12,
-  bg3       = dark_fg14,
-  fg0       = "#faf0fa",        
-  fg1       = "#e6e6fa",
-  fg2       = "#dda0dd",
-  fg3       = "#d8bfd8",
-  fg4       = "#ee82ee",
-  fg5       = "#add8e6",
-  accent0   = "#ff1493",
-  accent1   = "#00ffff",
-  borders   = "#add8e6",
-  muted     = "#4682b4",
-}
-]]
-
+--- little note : bg is for background and fg foreground
+---
+--- color names try to be |       comments give examples
+--- objective description |
+--- of what the color
+--- applies to
+---
+---@class Palette
 local ret = {
-  bg = dark_fg1,
-  bg_dark = "#1f2335",
-  bg_dark1 = "#1b1e2d",
-  bg_highlight = "#292e42",
-  fg1 = "#7aa2f7",
-  fg10 = "#3d59a1",
-  fg_pop1 = "#2ac3de",
-  fg12 = "#0db9d7",
-  fg2 = "#89ddff",
-  fg3 = "#b4f9f8",
-  fg4 = "#394b70",
-  comment = "#565f89",
-  fg_pop2 = "#7dcfff",
-  fg_dim1 = "#545c7e",
-  fg_dim2 = "#737aa2",
-  fg = "#c0caf5",
-  fg_dim3 = "#a9b1d6",
-  fg_dim4 = "#3b4261",
-  fg5 = "#9ece6a",
-  fg6 = "#73daca",
-  green2 = "#41a6b5",
-  fg_pop3 = "#bb9af7",
-  fg7 = "#ff007c",
-  fg_accent = "#ff9e64",
-  fg8 = "#9d7cd8",
-  fg9 = "#f7768e",
-  red1 = "#db4b4b",
-  fg10 = "#1abc9c",
-  terminal_black = "#414868",
-  fg_accent2 = "#e0af68",
+  bg = "#000717",             --regular background
+  bg_dark = "#000717",        --background in inactive window
+  bg_highlight = "#000717",   --background for current line
+
+  fg = "#f0f8ff",             --regular foreground
+  fg1 = "#ccfdfd",            --fg for function and methods name
+  fg2 = "#f0f8ff",            --fg for operators
+  fg3 = "#f0f8ff",            --fg for regexes
+  fg4 = "#f0f8ff",            --bg for lsp inlay hints
+  fg5 = "#f993ca",            --characters and strings
+  fg6 = "#16baba",            --fg for healthSuccess
+  fg7 = "#f0f8ff",            --foo
+  fg8 = "#850a4a",            --fg for treesitter keyword
+  fg9 = "#f7768e",            --fg for replacement text highlighing or treesitter builtin variable names (this)
+  fg10 = "#1abc9c",           --fg for treesitter markup link
+
+  fg_pop1 = "#20e5e5",        --fg for types
+  fg_pop2 = "#4d092c",        --fg fork keywords and preproc
+  fg_pop3 = "#ee198b",        --fg for statements
+
+  fg_accent = "#16baba",      --various accent colors (current line number, matching parenthesis...)
+  fg_accent2 = "#e0af68",     --things needing extra attention like the keyword TODO, FIXME...
+
+  fg_dim1 = "#545c7e",        --fg for NonText and SpecialKey
+  fg_dim2 = "#286395",        --fg for Conceal and quickfix line numbers
+  fg_dim3 = "#a9b1d6",        --mode messages (-- INSERT --) and punctuation
+  fg_dim4 = "#3b4261",        --dimmed stuff like line number
+  
+  comment = "#4782b4",        --fg for comments
+  terminal_black = "#414868", --read the code if you are interested in this, it does not look interesting to me
+
   git = {
     add = "#449dab",
     change = "#6183bb",
     delete = "#914c54",
   },
-}
 
+  --used sparsely in some plugins
+  fg11 = "#0db9d7",
+  green2 = "#41a6b5",
+  red1 = "#db4b4b",
+}
 return ret
