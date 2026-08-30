de1fdc5e5a2581ae4a6c1367130c20e4f366518d
SeeTwoo <waltibee@gmail.com>
2026-08-30 16:39:30 +0200

    save

--- lua/tokyonight/colors/neonight.lua
+++ lua/tokyonight/colors/neonight.lua
@@ -8,7 +8,7 @@
 ---@class Palette
 local ret = {
   bg = "#000717",             --regular background
-  bg_dark = "#001033",        --background in inactive window
+  bg_dark = "#000c29",        --background in inactive window
   bg_highlight = "#000717",   --background for current line
 
   fg = "#f0f8ff",             --regular foreground
